13d835d958e9c2d1884c581199b90ba774bfafea
David M. Butler <d.m.butler@limitpoint.com>
2013-01-19 00:03:53 -0800

    Backup.

--- examples/sheaf/example3.cc
+++ examples/sheaf/example3.cc
@@ -17,49 +17,69 @@ using namespace sheaf;
 
 int main( int argc, char* argv[])
 {
-  // Preconditions:
-  
-  // Body:
-
   // Concurrency control enabled by default.
 
   // Create a standard sheaves namespace.
 
-  sheaves_namespace lns("Hello-sheaf!");
+  sheaves_namespace* lns = new sheaves_namespace("Hello-sheaf");
 
   // Write its name to cout.
   // Requires read access to the namespace.
   
   // Be polite, request access.
+  // If another client has read-write access, execution will block
+  // until it releases access, if threads are enabled. Otherwise,
+  // the request will succeed immediately.
 
-  lns.get_read_access();
+  lns->get_read_access();
+
+  // Can nest requests as deep as you want,
+  // or at least until the integer depth counter
+  // overflows.
+
+  cout << "access request depth " << lns->access_request_depth() << endl;
+  lns->get_read_access();
+  cout << "access request depth " << lns->access_request_depth() << endl;
 
   // Invoke the operation.
 
-  cout << lns.name();
+  cout << lns->name();
 
-  // Be proper, release access.
+  // Be proper, release access so this client
+  // or another can get write access.
+  // Have to match every request with a release.
  
-  lns.release_access();
+  lns->release_access();
+  lns->release_access();
+
+  // Delete the namespace, requires read-write access.
+  // Be polite, request access. If another client has 
+  // either read or read-write access, execution will 
+  // block until it releases access, if threads are enabled. 
+  // Otherwise, the request will succeed immediately.
+  // This client must not already have read-only access,
+  // see precondition for details.
+
+  lns->get_read_write_access(false);
   
-  // Alternately can use version with auto access.
-  // Will request and release access internally, 
-  // if client requests it by setting auto access
-  // argument to true.
+  // Invoke the operation.
   
-  cout << lns.name(true);
+  delete lns;
+  
+  // Deletion is the only case where the client
+  // can not be proper and release access.
+  
+  // Create another namespace.
+
+  lns = new sheaves_namespace("Hello-sheaf");
 
   // Invoking a function that requires access
   // without first getting access violates the
   // precondition of the function.
   // the following will throw an exception and abort.
 
-  cout << lns.name();
+  cout << lns->name();
   
-  // Postconditions:
-  
-  // Exit:
-
   return 0;
 }
 
