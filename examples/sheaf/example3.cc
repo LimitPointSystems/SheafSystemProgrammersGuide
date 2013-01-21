9fb9215e5d3fa37fd629e77acd60db4cbe15bf25
David M. Butler <d.m.butler@limitpoint.com>
2013-01-20 21:15:10 -0800

    xfr leafhead -> stones

--- examples/sheaf/example3.cc
+++ examples/sheaf/example3.cc
@@ -5,11 +5,9 @@
 // Copyright (c) 2008 Limit Point Systems, Inc. 
 //
 
-/// @example Example3: Concurrency access control enforced by preconditions.
-/// SheafSystem Programmer's Guide Example 3. Concurrency access control enforced by preconditions
+/// @example Example3: Manual concurrency access control.
+/// SheafSystem Programmer's Guide Example 3: Manual concurrency access control. 
 
-#include "assert_contract.h"
-#include "error_message.h"
 #include "sheaves_namespace.h"
 #include "std_iostream.h"
 
@@ -21,21 +19,21 @@ int main( int argc, char* argv[])
 
   // Create a standard sheaves namespace.
 
-  sheaves_namespace* lns = new sheaves_namespace("Hello-sheaf");
+  sheaves_namespace* lns = new sheaves_namespace("Example3A");
 
   // Write its name to cout.
   // Requires read access to the namespace.
   
   // Be polite, request access.
-  // If another client has read-write access, execution will block
-  // until it releases access, if threads are enabled. Otherwise,
-  // the request will succeed immediately.
+  // If and threads are enabled and another thread has 
+  // read-write access, execution will block until it 
+  // releases access. Otherwise, the request will succeed 
+  // immediately.
 
   lns->get_read_access();
 
-  // Can nest requests as deep as you want,
-  // or at least until the integer depth counter
-  // overflows.
+  // Can nest requests as deep as you want, or at least 
+  // until the integer depth counter overflows.
 
   cout << "access request depth " << lns->access_request_depth() << endl;
   lns->get_read_access();
@@ -43,9 +41,9 @@ int main( int argc, char* argv[])
 
   // Invoke the operation.
 
-  cout << lns->name();
+  cout << lns->name() << endl;
 
-  // Be proper, release access so this client
+  // Be proper, release access so this thread
   // or another can get write access.
   // Have to match every request with a release.
  
@@ -53,9 +51,9 @@ int main( int argc, char* argv[])
   lns->release_access();
 
   // Delete the namespace, requires read-write access.
-  // Be polite, request access. If another client has 
-  // either read or read-write access, execution will 
-  // block until it releases access, if threads are enabled. 
+  // Be polite, request access. If threads are enabled
+  // and another thread has either read or read-write 
+  // access, execution will block until it releases access. 
   // Otherwise, the request will succeed immediately.
   // This client must not already have read-only access,
   // see precondition for details.
@@ -71,14 +69,14 @@ int main( int argc, char* argv[])
   
   // Create another namespace.
 
-  lns = new sheaves_namespace("Hello-sheaf");
+  lns = new sheaves_namespace("Example3B");
 
   // Invoking a function that requires access
   // without first getting access violates the
   // precondition of the function.
-  // the following will throw an exception and abort.
+  // The following will throw an exception and abort.
 
-  cout << lns->name();
+  cout << lns->name() << endl;
   
   return 0;
 }
