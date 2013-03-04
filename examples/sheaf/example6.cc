fc94ad9dc99dd72cc7a571f7ee785f23094408cd
David M. Butler <d.m.butler@limitpt.com>
2013-03-04 15:06:55 -0800

    backup.

---
+++ examples/sheaf/example6.cc
@@ -0,0 +1,33 @@
+// $RCSfile: main.t.cc,v $ $Revision: 1.1 $ $Date: 2009/07/31 18:01:41 $
+
+// $Name:  $
+//
+// Copyright (c) 2013 Limit Point Systems, Inc. 
+//
+
+/// @example Example6
+/// SheafSystem Programmer's Guide Example 5. Write a namespace to a file. 
+
+#include "sheaves_namespace.h"
+#include "std_iostream.h"
+
+using namespace sheaf;
+
+int main( int argc, char* argv[])
+{  
+  cout << "SheafSystemProgrammersGuide Example6:" << endl;
+
+  // Create a namespace.
+
+  sheaves_namespace lns("Example6");
+
+  // Write the namespace to cout.
+
+  cout << lns << endl;
+  
+  // Exit:
+
+  return 0;
+}
+
+  
