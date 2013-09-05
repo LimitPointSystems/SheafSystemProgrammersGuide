2a505b0bf96a232b147337ab918f56e53c76abc9
James E. Butler <j.e.butler@limitpoint.com>
2013-09-05 07:58:33 -0700

    Cleaned up file headers and added Apache copyright notice throught project.

--- examples/cmake_modules/FindHDF5.cmake
+++ examples/cmake_modules/FindHDF5.cmake
@@ -1,9 +1,20 @@
+
 #
+# Some Portions Copyright (c) 2013 Limit Point Systems, Inc. 
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# Some portions Copyright (c) 2013 Limit Point Systems, Inc.
+#   http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
+
 # - Find HDF5, a library for reading and writing self describing array data.
 #
 # This module invokes the HDF5 wrapper compiler that should be installed
